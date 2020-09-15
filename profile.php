4be8b99449a08c9c0daf4a7e2e94189f9f0e84c9
Lateralny <59287936+Lateralny@users.noreply.github.com>
2020-09-15 20:29:38 +0200

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,48 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../public/css/style.css"/>
+    <title> flamingo </title>
+</head>
+<body>
+<header>
+    <nav>
+
+        <div class="tab-container">
+            <div class="home">
+                 <img src="../public/img/home.svg" alt="submit">
+            </div>
+
+            <div class="search">
+                <img src="../public/img/search.svg" alt="submit">
+            </div>
+
+            <div class="new_post">
+                <img src="../public/img/new_post.svg" alt="submit">
+            </div>
+
+            <div class="settings">
+                <img src="../public/img/settings.svg" alt="submit">
+            </div>
+
+            <div class="search">
+                <img src="../public/img/avatar_toolbar.svg" alt="submit">
+            </div>
+        </div>
+        <form>
+        </form>
+
+    </nav>
+</header>
+    <div class="container">
+        <div class="avatar_big"></div>
+        <div class="line"></div>
+        <div class="tagged"></div>
+        <div class="view_mode"></div>
+        <div class="profile_nickname">amanda</div>
+    </div>
+</body>
+</html>
+
